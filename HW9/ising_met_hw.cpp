872ef79931e5d22b4b522f567313e9dd8c0bb37a
bhattathakur <bhattathaur2015@gmail.com>
2020-11-07 23:00:38 -0500

    some addition

--- HW9/ising_met_hw.cpp
+++ HW9/ising_met_hw.cpp
@@ -35,23 +35,24 @@ public:
 
 class ISING_CONF
 {
-public:
-  ISING_CONF(const PARAMS&, const LATTICE&, MTRand&);//constructor
-  void conf_write(const PARAMS&, const LATTICE& latt);
-  void sweep(const PARAMS&,const LATTICE&,MTRand&);
-  void meas_clear(const PARAMS&,const LATTICE&,MTRand&);
-  void meas(const PARAMS&,const LATTICE&,MTRand&);
-  void binwrite(const PARAMS&,const LATTICE&,MTRand&);
-  ~ISING_CONF();// destructor
-private:
-  vector <int> spin;
-  ofstream dfout;
-  vector <vector<vector<double> > > wght_tbl;
-  // these are the observables
-  double energy;
-  double energy_sq;
-  double mag;
-  double mag_sq;
+  public:
+    ISING_CONF(const PARAMS&, const LATTICE&, MTRand&);//constructor
+    void conf_write(const PARAMS&, const LATTICE& latt);
+    void sweep(const PARAMS&,const LATTICE&,MTRand&);
+    void meas_clear(const PARAMS&,const LATTICE&,MTRand&);
+
+    void meas(const PARAMS&,const LATTICE&,MTRand&);
+    void binwrite(const PARAMS&,const LATTICE&,MTRand&);
+    ~ISING_CONF();// destructor
+  private:
+    vector <int> spin;
+    ofstream dfout;
+    vector <vector<vector<double> > > wght_tbl;
+    // these are the observables
+    double energy;
+    double energy_sq;
+    double mag;
+    double mag_sq;
 };
 
 int main(void)
@@ -63,50 +64,127 @@ int main(void)
 
   // latt.print();
   //EQUILIBRATE
-   for(int eql=0;eql<p.Neql;eql++)
-     ising.sweep(p,latt,ran1);
-  
+  for(int eql=0;eql<p.Neql;eql++)
+    ising.sweep(p,latt,ran1);
+
   //PRODUCTION
-   for (int bin=0;bin<p.Nbin;bin++)
-     {
-       ising.meas_clear(p,latt,ran1);
-       for(int mcs=0;mcs<p.Nmcs;mcs++)
-	 {
-	    ising.sweep(p,latt,ran1);
-	    ising.meas(p,latt,ran1);
-	 }
-       ising.binwrite(p,latt,ran1);
-     }
+  for (int bin=0;bin<p.Nbin;bin++)
+  {
+    ising.meas_clear(p,latt,ran1);
+    for(int mcs=0;mcs<p.Nmcs;mcs++)
+    {
+      ising.sweep(p,latt,ran1);
+      ising.meas(p,latt,ran1);
+    }
+    ising.binwrite(p,latt,ran1);
+  }
 }
 
 PARAMS::PARAMS(){
- // ---------COMPLETE CODE HERE--------!
+  // ---------COMPLETE CODE HERE--------!
+  //initializes commonly used parameters from a file
+  //int Nlin; // linear size of lattice
+  //double beta; // 1/T
+  //int Neql;// eql sweeps
+  //int Nmcs;// sweeps in a bin
+  //int Nbin;// number of bin
+  //int SEED; // for MT
+  //string latt_;// lattice kind
+  ifstream pfin;
+  pfin.open("param.dat");  
+  if (pfin.is_open()) { 
+    pfin >> Nlin;
+    pfin >> beta;
+    pfin >> Neql;
+    pfin >> Nmcs;
+    pfin >> Nbin;
+    pfin >> SEED;
+    pfin >> latt_;
+  }
+  else
+  {cout << "No input file to read ... exiting!"<<endl;exit(1);}
+  pfin.close();
+  // print out all parameters for record
+  cout << "---Input Parameters---"<<endl; 
+  cout <<"Nlin = "<<Nlin<<"; beta= "<<beta<<endl;
+  cout <<"Neql= "<<Neql<<"\t Nmcs\t"<<Nmcs<<"\t; Number of bins = "<<Nbin<<endl;
+  cout <<"RNG will be given SEED of = "<<SEED<<endl;
+  cout <<"Percolation problem on lattice --> "<<latt_<<endl;
 };//constructor
-
 
 LATTICE::LATTICE (const PARAMS& p)
 {
- // ---------COMPLETE CODE HERE--------!
-}
+  if(p.latt_=="sqlatt_PBC")
+  {
+    Lx=p.Nlin;Ly=p.Nlin;
+    Nsite=Lx*Ly;
+    for(int site=0;site<Nsite;site++)  //site runs from 0 to Nlin*Nlin	      
+    {
+      int x=site%Lx;                  //gives x of site
+      int y=site/Ly;                  //gives y of site
+      vector <int> nrn; 		//vector to hold neighbour elements
+      (x+1<Lx)?nrn.push_back(site+1):nrn.push_back(site+1-Lx);  //use of ternary operator
+      (y+1<Ly)?nrn.push_back(site+Ly):nrn.push_back(site-y*Ly);
+      (x-1>=0)?nrn.push_back(site-1):nrn.push_back(site-1+Lx);
+      (y-1>=0)?nrn.push_back(site-Ly):nrn.push_back(Nsite-Ly+site);
+      nrnbrs.push_back(nrn); 		//neighbour vector is added to the main vector nrnbrs
+    }
+  }
 
+  else if(p.latt_=="sqlatt_OBC")
+  {
+    Lx=p.Nlin;Ly=p.Nlin;
+    Nsite=Lx*Ly;
+    for(int site=0;site<Nsite;site++) 
+    {
+      int x=site%Lx;                   
+      int y=site/Ly;		        
+      vector <int> nrn;                
+      if(x+1<Lx)nrn.push_back(site+1);
+      if(y+1<Ly)nrn.push_back(site+Ly);
+      if(x-1>=0)nrn.push_back(site-1);
+      if(y-1>=0)nrn.push_back(site-Ly);
+      nrnbrs.push_back(nrn);         
+    }
+  }
+  else
+  {cout <<"Dont know your option for lattice in param.dat .. exiting"<<endl;exit(1);}
+}
 void LATTICE::print()
 {
- // ---------COMPLETE CODE HERE--------!
+  //THIS FUNCTIONS MAY BE CALLED DURING DEBUGGING TO MAKE SURE LATTICE HAS BEEN DEFINED CORRECTLY
+  cout <<"---printing out properties of lattice ---"<<endl;
+  cout<<"size is  "<<Lx<<"x"<<Ly<<endl;
+  cout <<"neighbors are"<<endl;
+  for (int site=0;site<Nsite;site++)
+  {
+    cout <<site<<" : ";
+    for (int nn=0;nn<nrnbrs.at(site).size();nn++)
+      cout<<nrnbrs.at(site).at(nn)<<" ";
+    cout <<endl;
+  }
 }
-
 
 ISING_CONF::ISING_CONF(const PARAMS& p, const LATTICE& latt, MTRand& ran1)
 {
   // INITIALIZE SPIN CONFIG
   // ---------COMPLETE CODE HERE--------!
 
+  //size of spin conf vector is Nsite
+  spin.resize(latt.Nsite);
+
+  //initilize the spin with 0 or 1
+  for (int i=0;i<latt.Nsite;i++)spin[i]=ran1.randInt(1);    //randInt(n)->0,..,n
+
   //CREATE WEIGHT TABLE
+  //possible sum of spin of neighbours of a site is 5 (0,1,2,3,4) 0->all down, 4->all up
+  //new and old both spin configuration have 2 possible values 0, 1
   if(p.latt_=="sqlatt_PBC")
-    {
-  // ---------COMPLETE CODE HERE--------!
-    }
+  {
+    // ---------COMPLETE CODE HERE--------!
+  }
   else
-    {cout <<"NEED TO CODE ALL LATTICE OPTIONS"<<endl;}
+  {cout <<"NEED TO CODE ALL LATTICE OPTIONS"<<endl;}
 }
 
 ISING_CONF::~ISING_CONF()
@@ -116,28 +194,28 @@ ISING_CONF::~ISING_CONF()
 
 void ISING_CONF::conf_write(const PARAMS& p, const LATTICE& latt)
 {
- // ---------COMPLETE CODE HERE--------!
+  // ---------COMPLETE CODE HERE--------!
 }
 
 
 void ISING_CONF::sweep(const PARAMS& p, const LATTICE& latt, MTRand& ran1)
 {
   /*SWEEP THROUGH THE LATTICE*/
- // ---------COMPLETE CODE HERE--------!
+  // ---------COMPLETE CODE HERE--------!
 }
 
 void ISING_CONF::meas_clear(const PARAMS& p, const LATTICE& latt, MTRand& ran1)
 {
-// ---------COMPLETE CODE HERE--------!
+  // ---------COMPLETE CODE HERE--------!
 }
 
 void ISING_CONF::meas(const PARAMS& p, const LATTICE& latt, MTRand& ran1)
 {
- // ---------COMPLETE CODE HERE--------!
+  // ---------COMPLETE CODE HERE--------!
 }
 
 void ISING_CONF::binwrite(const PARAMS& p, const LATTICE& latt, MTRand& ran1)
 {
-// ---------COMPLETE CODE HERE--------!
+  // ---------COMPLETE CODE HERE--------!
 }
 
